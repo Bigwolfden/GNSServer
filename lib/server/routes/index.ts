3a3e01df34ef6b829e544ced8f51b161307b6818
Ryan Wolf <ryantwolf1@gmail.com>
2020-02-18 14:16:25 -0500

    Fix thank you page

--- lib/server/routes/index.ts
+++ lib/server/routes/index.ts
@@ -15,10 +15,17 @@ router.get('/clients', async (req, res) => {
 router.post('/add', async (req, res) => {
     //Get the values from the body of the request
     const {first_name, last_name, phone_number, email, address, city, zip, state} = req.body;
-    const values = [first_name, last_name, phone_number, email, address, city, zip, state];
+    const values = [first_name, last_name, phone_number.substring(0,10), email, address, city, zip, state];
+    //Don't add to the database if any value is blank
+    for (let value of values) {
+        if (!value || value == "") {
+            res.sendStatus(200);
+            return;
+        }
+    }
     console.log(values);
     //Add them to the database
     await pool.query("INSERT INTO clients (first_name, last_name, phone, email, street_address, city, zip, state, date_added, stage) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, current_timestamp, 1);", values);
-    
+    //res.redirect("https://localhost:3000/#lastpage");
     res.sendStatus(200);
 });
